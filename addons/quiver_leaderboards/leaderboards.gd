6608234a97b0ec7a18558bb0a92c6a466bc16264
seb-navarro <navarrosebastian154@gmail.com>
2025-03-13 12:58:46 +0000

    Fixed handling of http response code "0" when not connected to the internet.

--- addons/quiver_leaderboards/leaderboards.gd
+++ addons/quiver_leaderboards/leaderboards.gd
@@ -127,6 +127,9 @@ func post_guest_score(leaderboard_id: String, score: float, nickname := "", meta
 				printerr("[Quiver Leaderboards] There was an irrecoverable error posting a score.")
 				retry = false
 				success = false
+			elif response_code == 0:
+				printerr("[Quiver Leaderboards] There was an error posting a score.")
+				success = false
 		_http_post_request_busy = false
 
 	if not success and retry:
